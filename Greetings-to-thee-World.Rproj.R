1aa735530c730c06cdacf2db300f75f545d26d48
GeraldBothe <Gerald.Bothe@ranchobiosciences.com>
2022-01-10 17:44:23 -0500

    Testing RStudion connection and commit

---
+++ Greetings-to-thee-World.Rproj.R
@@ -0,0 +1,3 @@
+print("Public repository. This file was created from within client area RStudio")
+print("This repository resides within GeraldBothe's professional area")
+Print('This file now lives in GitHub, which concludes the testing')
