9def54d7cba3b3ea33a6b936699a45f16629053b
GeraldBothe <Gerald.Bothe@ranchobiosciences.com>
2022-01-13 14:56:27 -0500

    include comments

--- Greetings-to-thee-World.Rproj.R
+++ Greetings-to-thee-World.Rproj.R
@@ -1,2 +1,13 @@
 print("Greetings to the World!")
 print("This file now lives in GitHub, which concludes the testing")
+#usr/pw authentication no longer works:
+#>>> C:/Program Files/Git/bin/git.exe push origin HEAD:refs/heads/main
+#Logon failed, use ctrl+c to cancel basic credential prompt.
+#remote: Support for password authentication was removed on August 13, 2021. Please use a personal access token instead.
+#remote: Please see https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/ for more information.
+#fatal: Authentication failed for 'https://github.com/GeraldBothe/Greetings-to-thee-World/'
+#updating the connection mechanism:
+install.packages("usethis")
+library(usethis)
+?use_github
+edit_r_environ()
